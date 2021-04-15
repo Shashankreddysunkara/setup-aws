fff421637eaee3da153ab284461d1b77bf69ee24
Shashank Reddy Sunkara <inusasunny@gmail.com>
2021-04-15 12:14:16 +0530

    Update variables.tf

--- terraform/vpc/tf_aws_vpc/variables.tf
+++ terraform/vpc/tf_aws_vpc/variables.tf
@@ -16,7 +16,7 @@ variable "private_subnets_cidr" {
 
 variable "azs" {
   description = "A list of Availability zones in the region"
-  type = map
+//  type = map
   default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
