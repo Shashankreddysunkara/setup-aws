9ba607830e40000e3560bca2a4ee05b6c5bcf1d5
Shashank Reddy Sunkara <inusasunny@gmail.com>
2021-04-15 12:19:30 +0530

    Update vpc.tf

--- terraform/vpc/vpc.tf
+++ terraform/vpc/vpc.tf
@@ -12,10 +12,10 @@ module "vpc" {
   cidr = "172.31.0.0/16"
 
   # TODO FIXME
-  private_subnets_cidr = ["172.31.32.0/20", "172.31.64.0/20", "172.31.96.0/20"]
+  private_subnets_cidr = "172.31.XXX.0/24"
 
   # TODO FIXME
-  public_subnets_cidr = ["172.31.16.0/20", "172.31.48.0/20", "172.31.80.0/20"]
+  public_subnets_cidr = "172.31.XXX.0/24"
 
   azs      = "${data.aws_availability_zones.available.names}"
   az_count = "${var.az_count}"
